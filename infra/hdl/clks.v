eac426704575ee46bd4878577656b136b9f98fd4
finnball <finn.ball@codethink.co.uk>
2017-10-04 10:00:27 +0100

    Simplified clk params. Added null input ports to remove iverilog warnings

--- infra/hdl/clks.v
+++ infra/hdl/clks.v
@@ -3,42 +3,36 @@ module clks(
 	    output clk_o
 	    );
 
-   parameter CLK_DIV = 8;
-
-   wire 	   s_clk_o;   
+   // Settings below default to clk_o = (clk_i / 8)
    
-   generate
-      if (CLK_DIV == 8)
-	begin
-	   
-	   SB_GB gb (
-		     .USER_SIGNAL_TO_GLOBAL_BUFFER(s_clk_o),
-		     .GLOBAL_BUFFER_OUTPUT (clk_o)
-		     );
-	   
-	   SB_PLL40_CORE #(
-			   .FEEDBACK_PATH("SIMPLE"),
-			   .PLLOUT_SELECT("GENCLK"),
-			   .DIVR(4'b0000),
-			   .DIVF(7'b0111111),
-			   .DIVQ(3'b011),  
-			   
-			   .FILTER_RANGE(3'b001)
-			   ) uut (
-				  .REFERENCECLK(clk_i),
-				  .PLLOUTCORE(s_clk_o),
-				  .RESETB(1'b1),
-				  .BYPASS(1'b0)
-				  );
-	   
-	end // if (CLK_SPEED == 8)
-      else
-	assign clk_o = clk_i;
-      
-   endgenerate
+   parameter DIVR = 4'b0000;
+   parameter DIVF = 7'b0111111;
+   parameter DIVQ = 3'b011;
+  
+   wire 	   s_clk_o;  
    
+   SB_GB gb (
+	     .USER_SIGNAL_TO_GLOBAL_BUFFER(s_clk_o),
+	     .GLOBAL_BUFFER_OUTPUT (clk_o)
+	     );
    
-
-
+   SB_PLL40_CORE #(
+		   .FEEDBACK_PATH("SIMPLE"),
+		   .PLLOUT_SELECT("GENCLK"),
+		   .DIVR(DIVR),
+		   .DIVF(DIVF),
+		   .DIVQ(DIVQ),  
+		   .FILTER_RANGE(3'b001)
+		   ) uut (
+			  .REFERENCECLK(clk_i),
+			  .PLLOUTCORE(s_clk_o),
+			  .RESETB(1'b1),
+			  .BYPASS(1'b0),
+			  .EXTFEEDBACK(1'b0),
+			  .DYNAMICDELAY(8'b00000000),
+			  .LATCHINPUTVALUE(1'b0),
+			  .SDI(1'b0),
+			  .SCLK(1'b0)
+			  );
    
 endmodule // clks
