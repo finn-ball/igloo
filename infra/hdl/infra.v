66c49978cc1d4ea25d35a2e77ce205c489e7750d
finnball <finn.ball@codethink.co.uk>
2017-10-09 17:24:12 +0100

    Fixed generate statement on fifo

--- infra/hdl/infra.v
+++ infra/hdl/infra.v
@@ -7,15 +7,13 @@ module infra(
 	     output [7:0] led_o,
 	     output 	  rs232_tx_o
 	     );
-
+   
    wire 		  clk_96;
    wire 		  clk_led;
    wire 		  clk_uart_rx, clk_uart_tx;
 
-   reg [3:0]			  ctr = 0;
-   reg [9:0] 		  rx_tmp = 10'b1010001110;
-   wire 		  tmp_rx_bit;
-   
+   wire [`UART_DATA_LENGTH - 1 : 0] rx_o, tx_i;
+   wire 			    rx_o_v, tx_i_v, tx_o_v;   
    
    clks #(
 	  .PLL_EN (1),
@@ -28,7 +26,7 @@ module infra(
    clks #(
 	  .PLL_EN(0),
 	  .GBUFF_EN(0),
-	  .T(`UART_CLK_RX_PERIOD * `UART_RX_SAMPLE_RATE)
+	  .T(`UART_CLK_RX_FREQ / `UART_RX_SAMPLE_RATE / 2)
 	  )clk_uart_rx_gen(
 			.clk_i (clk_i),
 			.clk_o (clk_uart_rx)
@@ -37,7 +35,7 @@ module infra(
    clks #(
 	  .PLL_EN(0),
 	  .GBUFF_EN(0),
-	  .T(`UART_CLK_TX_PERIOD)
+	  .T(`UART_CLK_TX_FREQ / 2)
 	  )clk_uart_tx_gen(
 			.clk_i (clk_i),
 			.clk_o (clk_uart_tx)
@@ -55,34 +53,33 @@ module infra(
    uart_ctrl uart_ctrl(
 		       .clk_rx_i (clk_uart_rx),
 		       .clk_tx_i (clk_uart_tx),
-		       .rst_i(rst_i),
-		       .rx_i (tmp_rx_bit),
-		       //.rx_i (rs232_rx_i),
-		       .tx_o (rs232_tx_o)
+		       .rx_i (rs232_rx_i),
+		       .rx_o (rx_o),
+		       .rx_o_v(rx_o_v),
+		       .tx_i(tx_i),
+		       .tx_i_v(tx_i_v),
+		       .tx_o (rs232_tx_o),
+		       .tx_o_v (tx_o_v)
 		       );
-    
-   
-   assign tmp_rx_bit = rx_tmp[ctr];
-   
-   always @ (posedge clk_uart_tx)
-     begin
-	if (ctr[3:0] == 9 | rst_i)
-	  begin
-	     ctr <= 0;
-	  end
-	else
-	  begin
-	     ctr <= ctr + 1;
-	  end
-     end
 
+   fifo#(
+	 .DATA_WIDTH(`UART_DATA_LENGTH)
+	 ) fifo(
+		.w_clk(clk_uart_rx),
+		.r_clk(clk_uart_tx),
+		.we(rx_o_v),
+		.d(rx_o),
+		.re(1'b0),
+		.q(tx_i),
+		.mask(16'b0)
+		);
+   
    reg rst_led;
    
    always @ (negedge rst_i)
      begin
 	rst_led <= 1;
      end // always @ clk_i
-   
    
    assign led_o[0] = clk_led; // Heartbeat
    assign led_o[1] = rst_i; // Resetting
