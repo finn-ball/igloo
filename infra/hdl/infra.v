636d8441f2a9d4b278f61307400e559ba52f3f47
finnball <finn.ball@codethink.co.uk>
2017-10-11 10:33:08 +0100

    Added loop back on the rx -> tx

--- infra/hdl/infra.v
+++ infra/hdl/infra.v
@@ -12,20 +12,22 @@ module infra(
    wire 		  clk_led;
    wire 		  clk_uart_rx, clk_uart_tx;
 
-   wire [`UART_DATA_LENGTH - 1 : 0] rx_o, tx_i;
-   wire 			    rx_o_v, tx_i_v, tx_o_v;   
+   wire [`UART_DATA_LENGTH - 1 : 0] rx_o;
+   wire 			    rx_o_v, tx_o_v;
+   reg [`UART_DATA_LENGTH - 1 : 0]  tx_i;
+   reg 				    tx_i_v = 0;   
    
    clks #(
 	  .PLL_EN (1),
-	  .GBUFF_EN(1)
+	  .GBUFF_EN(0)
 	  ) clk_96_gen(
 		       .clk_i (clk_i),
 		       .clk_o (clk_96)
 		       );
-   
+  
    clks #(
 	  .PLL_EN(0),
-	  .GBUFF_EN(0),
+	  .GBUFF_EN(1),
 	  .T(`UART_CLK_RX_FREQ / `UART_RX_SAMPLE_RATE / 2)
 	  )clk_uart_rx_gen(
 			.clk_i (clk_i),
@@ -34,7 +36,7 @@ module infra(
    
    clks #(
 	  .PLL_EN(0),
-	  .GBUFF_EN(0),
+	  .GBUFF_EN(1),
 	  .T(`UART_CLK_TX_FREQ / 2)
 	  )clk_uart_tx_gen(
 			.clk_i (clk_i),
@@ -62,30 +64,43 @@ module infra(
 		       .tx_o_v (tx_o_v)
 		       );
 
+   reg 				    fifo_re, fifo_we = 0;
+   reg [`UART_DATA_LENGTH - 1 : 0]  fifo_d;
+   wire [`UART_DATA_LENGTH - 1 : 0] fifo_q;
+   
+   wire 			    fifo_empty;
+   
    fifo#(
 	 .DATA_WIDTH(`UART_DATA_LENGTH)
 	 ) fifo(
 		.w_clk(clk_uart_rx),
 		.r_clk(clk_uart_tx),
-		.we(rx_o_v),
-		.d(rx_o),
-		.re(1'b0),
-		.q(tx_i),
+		.we(fifo_we),
+		.d(fifo_d),
+		.re(fifo_re),
+		.q(fifo_q),
+		.empty(fifo_empty),
 		.mask(16'b0)
-		);
+		);   
    
-   reg rst_led;
    
-   always @ (negedge rst_i)
+   always @ (posedge clk_uart_rx)
      begin
-	rst_led <= 1;
-     end // always @ clk_i
+	fifo_we <= rx_o_v;
+	fifo_d <= rx_o;
+     end
+   
+   always @ (posedge clk_uart_tx)
+     begin
+	fifo_re <= ~fifo_empty & ~tx_o_v & ~tx_i_v;
+	tx_i <= fifo_q;
+	tx_i_v <= fifo_re;
+     end
    
    assign led_o[0] = clk_led; // Heartbeat
    assign led_o[1] = rst_i; // Resetting
-   assign led_o[2] = rst_led; // Finished Reset
    
-   assign led_o[7:4] = 0;
+   assign led_o[7:2] = 0;
    
 endmodule // infra
 
